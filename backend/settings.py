2a51e3f952101ef3db098224ab9f55a3bd61fa03
NwabuezeFranklin <nwafrank2017@gmail.com>
2023-06-14 10:23:54 +0100

    deployment

--- backend/settings.py
+++ backend/settings.py
@@ -193,6 +193,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'static'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
