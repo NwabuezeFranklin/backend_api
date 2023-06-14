57a19f2a6c972ae6fa36eb7f5a258be0ee440887
NwabuezeFranklin <nwafrank2017@gmail.com>
2023-06-14 09:26:09 +0100

    project base

---
+++ backend/urls.py
@@ -0,0 +1,23 @@
+# backend/urls.py
+
+from django.contrib import admin
+from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from App.views import Redirector
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include("api.urls")),
+    path('',include('App.urls')),
+    path('<str:shortener_link>/',Redirector.as_view(),name='redirector'),
+    # path('ckeditor/', include('ckeditor_uploader.urls')),
+    # path("ckeditor5/", include('django_ckeditor_5.urls')),
+
+]
+
+
+urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
