cb97e539d18e9127b646cc7a16eca2f242677fc2
NwabuezeFranklin <nwafrank2017@gmail.com>
2023-06-15 15:17:48 +0100

    Added Analytics

--- App/models.py
+++ App/models.py
@@ -8,6 +8,7 @@ class Link(models.Model):
     original_link=models.CharField(max_length=2550)
     shortened_link=models.URLField(blank=True,null=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    count = models.PositiveIntegerField(default=0, editable=False)
 
 
     def shortener(self):
@@ -19,9 +20,18 @@ class Link(models.Model):
                 break
 
         return new_link
+    
+    
+    def get_count(self):
+        return self.count
 
     def save(self,*args, **kwargs):
         if not self.shortened_link:
             new_link=self.shortener()
             self.shortened_link=new_link
         return super().save(*args, **kwargs)
+    
+    
+    def increase_count(self):
+        self.count += 1
+        self.save()
