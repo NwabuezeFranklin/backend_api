c9a6c793d8d0c3d6c892aa4331e5728d2ac2b3c7
NwabuezeFranklin <nwafrank2017@gmail.com>
2023-07-01 06:38:41 +0100

    Adjusting

--- backend/urls.py
+++ backend/urls.py
@@ -6,9 +6,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from App.views import Redirector
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from api import views
+# from rest_framework import permissions
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
 
 
 
@@ -19,7 +20,8 @@ urlpatterns = [
     path('',include('App.urls')),
     path('<str:shortener_link>',Redirector.as_view(),name='redirector'),
     path('auth/',include('drf_social_oauth2.urls',namespace='drf')),
-    path('accounts/', include('allauth.urls'))
+    path('accounts/', include('allauth.urls')),
+    path('google/login/callback/', views.google_login_callback, name='google_login_callback'),
     # path('ckeditor/', include('ckeditor_uploader.urls')),
     # path("ckeditor5/", include('django_ckeditor_5.urls')),
 
